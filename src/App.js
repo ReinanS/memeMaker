009acedae22fbf8a0331cd573a282004db86de13
Reinan <reinandesouza01@gmail.com>
2020-06-11 16:36:19 -0300

    Arquivos Iniciais

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>MemeMaker</h1>
+    </>
+    
+   
+  );
+}
+  
+export default App;
